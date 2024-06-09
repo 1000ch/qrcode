df21e4e582d912f01ab435f2fa43d6262bb3124b
1000ch <shogosensui@gmail.com>
2024-06-09 16:44:33 +0900

    Use global await

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 import {documentReady} from 'https://unpkg.com/html-ready';
 import {BrowserQRCodeSvgWriter} from 'https://unpkg.com/@zxing/library?module';
 
-documentReady.then(() => {
-  const params = new URLSearchParams(location.search);
-  const text  = document.querySelector('#text');
-  const qrcode = document.querySelector('#qrcode');
+await documentReady;
 
-  text.value = params.get('text') ?? null;
+const params = new URLSearchParams(location.search);
+const text = document.querySelector('#text');
+const qrcode = document.querySelector('#qrcode');
 
-  generateCode();
+text.value = params.get('text') ?? null;
 
-  function generateCode() {
-    if (text.value.length === 0) {
-      qrcode.alt = '';
-      qrcode.src = '';
-      return;
-    }
-
-    const qrcodeWriter = new BrowserQRCodeSvgWriter();
-    const svg = qrcodeWriter.write(text.value, 256, 256);
-    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    qrcode.alt = text.value;
-    qrcode.src = `data:image/svg+xml,${encodeURIComponent(svg.outerHTML.replaceAll(/\r?\n/g, ''))}`;
+function generateCode() {
+  if (text.value.length === 0) {
+    qrcode.alt = '';
+    qrcode.src = '';
+    return;
   }
 
-  text.addEventListener('input', () => {
-    generateCode();
-  });
+  const qrcodeWriter = new BrowserQRCodeSvgWriter();
+  const svg = qrcodeWriter.write(text.value, 256, 256);
+  svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  qrcode.alt = text.value;
+  qrcode.src = `data:image/svg+xml,${encodeURIComponent(svg.outerHTML.replaceAll(/\r?\n/g, ''))}`;
+}
+
+text.addEventListener('input', () => {
+  generateCode();
 });
+
+generateCode();
